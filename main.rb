27b5a8ae3b4e0862c8da6fec9ac814b9cd94e3f5
Gyanesh Pandeya <gyanesh.pandeya@gmail.com>
2017-07-25 17:45:06 +0000

    added showInitials API

--- main.rb
+++ main.rb
@@ -2,4 +2,10 @@ module Puzzle
     def Puzzle.add(a,b)
         a + b
     end
+    
+    def Puzzle.showInitials(str)
+         
+        str.upcase.split.map {|word| word[0]}.join
+      
+    end
 end
--- test.rb
+++ test.rb
@@ -1,3 +1,6 @@
 require_relative "./main"
 
-p Puzzle.add(3,4)
+# p Puzzle.add(3,4)
+
+p Puzzle.showInitials("test me again")
+
