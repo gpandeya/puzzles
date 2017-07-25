0dfe03975a16c30cc30f98965487d3d7ae2b770b
Gyanesh Pandeya <gyanesh.pandeya@gmail.com>
2017-07-25 18:46:21 +0000

    added expand API

--- main.rb
+++ main.rb
@@ -8,4 +8,14 @@ module Puzzle
         str.upcase.split.map {|word| word[0]}.join
       
     end
+    
+    def Puzzle.expand(inputInt)
+        inputStr = inputInt.to_s
+        newIntArray = inputStr.to_s.split("").map{|w| w.to_i}
+        temp = newIntArray.map.with_index{|num,pos| num*(10**(newIntArray.length-1-pos))}
+        temp.map{|number| number.to_s}
+        temp.join(" + ")
+          
+    end
+        
 end
--- test.rb
+++ test.rb
@@ -2,5 +2,6 @@ require_relative "./main"
 
 # p Puzzle.add(3,4)
 
-p Puzzle.showInitials("test me again")
+# p Puzzle.showInitials("test me again")
 
+p Puzzle.expand(14567)
